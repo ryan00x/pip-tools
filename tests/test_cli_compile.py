ryan00x/pip-tools

commit 7ce2ced84d9971c263c202375cb9de1915a120b9

Treat `--upgrade-packages` PKGSPECs as constraints (not just minimums), consistently (#1578)
* Also treat --upgrade-packages as '-c constraints.txt' style constraints

Ensure temporary constraints file gets cleaned up
in a way that avoids Windows file handling complications

* Don't add upgrade pkgs which are also existing pins as regular reqs

Remove leftovers of now unused existing_pins_to_upgrade set

* Parametrize test_upgrade_packages_option_subdependency

... to cover when the subdependency is not already pinned

* Parametrize combined --upgrade and -P test

... to cover when the -P pkgs are only subdependencies

piptools/scripts/compile.py
@@ -425,10 +425,7 @@ def cli(
         key_from_ireq(install_req): install_req for install_req in upgrade_reqs_gen
     }
 
-    existing_pins_to_upgrade = set()
-
-    # Exclude packages from --upgrade-package/-P from the existing
-    # constraints, and separately gather pins to be upgraded
+    # Exclude packages from --upgrade-package/-P from the existing constraints
     existing_pins = {}
 
     # Proxy with a LocalRequirementsRepository if --upgrade is not specified
@@ -446,9 +443,7 @@ def cli(
 
         for ireq in filter(is_pinned_requirement, ireqs):
             key = key_from_ireq(ireq)
-            if key in upgrade_install_reqs:
-                existing_pins_to_upgrade.add(key)
-            else:
+            if key not in upgrade_install_reqs:
                 existing_pins[key] = ireq
         repository = LocalRequirementsRepository(
             existing_pins, repository, reuse_hashes=reuse_hashes
@@ -515,6 +510,27 @@ def cli(
                 )
             )
 
+    if upgrade_packages:
+        constraints_file = tempfile.NamedTemporaryFile(mode="wt", delete=False)
+        constraints_file.write("\n".join(upgrade_packages))
+        constraints_file.flush()
+        try:
+            reqs = list(
+                parse_requirements(
+                    constraints_file.name,
+                    finder=repository.finder,
+                    session=repository.session,
+                    options=repository.options,
+                    constraint=True,
+                )
+            )
+        finally:
+            constraints_file.close()
+            os.unlink(constraints_file.name)
+        for req in reqs:
+            req.comes_from = None
+        constraints.extend(reqs)
+
     extras = tuple(itertools.chain.from_iterable(ex.split(",") for ex in extras))
 
     if extras and not setup_file_found:
@@ -525,9 +541,8 @@ def cli(
         key_from_ireq(ireq) for ireq in constraints if not ireq.constraint
     }
 
-    allowed_upgrades = primary_packages | existing_pins_to_upgrade
     constraints.extend(
-        ireq for key, ireq in upgrade_install_reqs.items() if key in allowed_upgrades
+        ireq for key, ireq in upgrade_install_reqs.items() if key in primary_packages
     )
 
     constraints = [req for req in constraints if req.match_markers(extras)]

tests/test_cli_compile.py
@@ -894,13 +894,20 @@ def test_upgrade_packages_version_option_no_existing_file(pip_conf, runner):
     assert "small-fake-b==0.2" in out.stderr
 
 
-def test_upgrade_packages_version_option_and_upgrade(pip_conf, runner):
+@pytest.mark.parametrize(
+    "reqs_in",
+    (
+        pytest.param("small-fake-a\nsmall-fake-b", id="direct reqs"),
+        pytest.param("small-fake-with-unpinned-deps", id="parent req"),
+    ),
+)
+def test_upgrade_packages_version_option_and_upgrade(pip_conf, runner, reqs_in):
     """
     piptools respects --upgrade-package/-P inline list with specified versions
     whilst also doing --upgrade.
     """
     with open("requirements.in", "w") as req_in:
-        req_in.write("small-fake-a\nsmall-fake-b")
+        req_in.write(reqs_in)
     with open("requirements.txt", "w") as req_in:
         req_in.write("small-fake-a==0.1\nsmall-fake-b==0.1")
 
@@ -1895,6 +1902,7 @@ def test_unreachable_index_urls(runner, cli_options, expected_message):
     assert expected_message in stderr_lines
 
 
+@pytest.mark.parametrize("subdep_already_pinned", (True, False))
 @pytest.mark.parametrize(
     ("current_package", "upgraded_package"),
     (
@@ -1903,7 +1911,7 @@ def test_unreachable_index_urls(runner, cli_options, expected_message):
     ),
 )
 def test_upgrade_packages_option_subdependency(
-    pip_conf, runner, current_package, upgraded_package
+    pip_conf, runner, current_package, upgraded_package, subdep_already_pinned
 ):
     """
     Test that pip-compile --upgrade-package/-P upgrades/downgrades subdependencies.
@@ -1914,7 +1922,8 @@ def test_upgrade_packages_option_subdependency(
 
     with open("requirements.txt", "w") as reqs:
         reqs.write("small-fake-a==0.1\n")
-        reqs.write(current_package + "\n")
+        if subdep_already_pinned:
+            reqs.write(current_package + "\n")
         reqs.write("small-fake-with-unpinned-deps==0.1\n")
 
     out = runner.invoke(

commit 327898596ede742578ed42532d21dd08578049fe

Fix leak of temp files when reading from stdin
When this code was originally written (#481), the tempfile name itself
was being added to the `constraints` list. It therefore needed to persist
until processing of that list was done.

That's no longer the case (the file gets parsed by `pip-tools` itself),
so there's no reason not to use the context manager interface which
guarantees proper cleanup.

In order to test, we need to mock over NamedTemporaryFile, which is
slightly fragile, but better than not having any testing at all.

A similar usage -- without the file leak -- was introduced in #1578, and
is therefore also updated here to use the contextmanager version of
NamedTemporaryFile. Because it was already mostly correct, no new test is
included for that behavior.

piptools/scripts/compile.py
@@ -354,20 +354,19 @@ def cli(
         if src_file == "-":
             # pip requires filenames and not files. Since we want to support
             # piping from stdin, we need to briefly save the input from stdin
-            # to a temporary file and have pip read that.  also used for
-            # reading requirements from install_requires in setup.py.
-            tmpfile = tempfile.NamedTemporaryFile(mode="wt", delete=False)
-            tmpfile.write(sys.stdin.read())
-            tmpfile.flush()
-            reqs = list(
-                parse_requirements(
-                    tmpfile.name,
-                    finder=repository.finder,
-                    session=repository.session,
-                    options=repository.options,
-                    comes_from_stdin=True,
+            # to a temporary file and have pip read that.
+            with tempfile.NamedTemporaryFile(mode="wt") as tmpfile:
+                tmpfile.write(sys.stdin.read())
+                tmpfile.flush()
+                reqs = list(
+                    parse_requirements(
+                        tmpfile.name,
+                        finder=repository.finder,
+                        session=repository.session,
+                        options=repository.options,
+                        comes_from_stdin=True,
+                    )
                 )
-            )
             constraints.extend(reqs)
         elif is_setup_file:
             setup_file_found = True
@@ -415,10 +414,9 @@ def cli(
         )
 
     if upgrade_packages:
-        constraints_file = tempfile.NamedTemporaryFile(mode="wt", delete=False)
-        constraints_file.write("\n".join(upgrade_packages))
-        constraints_file.flush()
-        try:
+        with tempfile.NamedTemporaryFile(mode="wt") as constraints_file:
+            constraints_file.write("\n".join(upgrade_packages))
+            constraints_file.flush()
             reqs = list(
                 parse_requirements(
                     constraints_file.name,
@@ -428,11 +426,8 @@ def cli(
                     constraint=True,
                 )
             )
-        finally:
-            constraints_file.close()
-            os.unlink(constraints_file.name)
-        for req in reqs:
-            req.comes_from = None
+            for req in reqs:
+                req.comes_from = None
         constraints.extend(reqs)
 
     extras = tuple(itertools.chain.from_iterable(ex.split(",") for ex in extras))

tests/test_cli_compile.py
@@ -9,6 +9,7 @@ import shlex
 import shutil
 import subprocess
 import sys
+import tempfile
 import typing as _t
 from textwrap import dedent
 from unittest import mock
@@ -1472,6 +1473,53 @@ def test_stdin(pip_conf, runner):
         input="small-fake-a==0.1",
     )
 
+    assert out.stdout == dedent("""\
+        small-fake-a==0.1
+            # via -r -
+        """)
+
+
+def test_tmpfile_for_stdin_is_cleaned_up(pip_conf, runner):
+    """
+    Test that when compiling requirements from stdin, a tempfile gets written with
+    those requirements and cleaned up.
+    """
+    tmpfile_name = None
+
+    # save the real constructor so that it can be used while `mock.patch()` is
+    # active -- also, it's a function, not a class, so we can't inherit from it
+    real_constructor = tempfile.NamedTemporaryFile
+
+    # a "spy" which can be mocked into place for `NamedTemporaryFile` to
+    # replace the implementation with one which has side-effects and makes test
+    # assertions
+    #
+    # this spy ensures that the file is deleted on exit
+    # it also sets a nonlocal to indicate that it was initialized at all
+    class NamedTempfileSpy:
+        def __init__(self, *args, **kwargs):
+            self._tmpfile = real_constructor(*args, **kwargs)
+
+        def __enter__(self):
+            ret = self._tmpfile.__enter__()
+            nonlocal tmpfile_name
+            tmpfile_name = self._tmpfile.name
+            return ret
+
+        def __exit__(self, *args, **kwargs):
+            assert os.path.exists(self._tmpfile.name)
+            ret = self._tmpfile.__exit__(*args, **kwargs)
+            assert not os.path.exists(self._tmpfile.name)
+            return ret
+
+    with mock.patch("tempfile.NamedTemporaryFile", NamedTempfileSpy):
+        out = runner.invoke(
+            cli,
+            ["-", "--output-file", "-", "--quiet", "--no-emit-options", "--no-header"],
+            input="small-fake-a==0.1",
+        )
+
+    assert tmpfile_name is not None  # ensure the spy was used
     assert out.stdout == dedent("""\
         small-fake-a==0.1
             # via -r -